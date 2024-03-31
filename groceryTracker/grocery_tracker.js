98eb53abafe028672db606f02c9179e323e6e7c2
themousleh <ialmoslih@gmail.com>
2024-03-31 10:33:41 -0400

    new

--- groceryTracker/grocery_tracker.js
+++ groceryTracker/grocery_tracker.js
@@ -3,13 +3,11 @@ let groery2;
 let groery3;
 
 function calculateTotalAmount(){
-    groery1 = parseFloat(document.getElementById('groery1').value);
-    groery2 = parseFloat(document.getElementById('groery2').value);
-    groery3 = parseFloat(document.getElementById('groery3').value);
-
+    groery1 = parseFloat(document.getElementById('grocery1').value);
+    groery2 = parseFloat(document.getElementById('grocery2').value);
+    groery3 = parseFloat(document.getElementById('grocery3').value);
 
     let totalamount= groery1+groery2+groery3;
 
-    document.getElementById('result').innerText= `The total amount is:$ ${totalamount}`;
-
+    document.getElementById('result').innerText= `The total amount is: $${totalamount}`;
 }
