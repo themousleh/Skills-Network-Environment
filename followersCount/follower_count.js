67e2cb15db3c5ec997a4a24102aefc626f6ad8e7
themousleh <ialmoslih@gmail.com>
2024-03-31 11:19:21 -0400

    iam back

--- followersCount/follower_count.js
+++ followersCount/follower_count.js
@@ -9,10 +9,9 @@ function displayCount() {
     document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
     }
 
-function checkCountValue() {
-        if (count === 10) {
-          alert("Your Instagram post gained 10 followers! Congratulations!");
-        } else if (count === 20) {
-          alert("Your Instagram post gained 20 followers! Keep it up!");
+    function checkCountValue() {
+        if (count % 10 === 0 && count !== 0) {
+            alert("Your Instagram post gained " + count + " followers! Congratulations!");
         }
-      }
+    }
+    
